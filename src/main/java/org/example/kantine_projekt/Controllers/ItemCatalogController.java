4a4bf3ee8b16759bf02f6a4f00969306b283d548
Jakob2rune <jakjes01@easv365.dk>
2025-04-23 19:54:16 +0200

    Small Changes to the Architecture

---
+++ src/main/java/org/example/kantine_projekt/Controllers/ItemCatalogController.java
@@ -0,0 +1,39 @@
+package org.example.kantine_projekt.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ItemCatalogController {
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void initialize() {}
+
+    public void addNewItem(){}
+
+    public void updateItem(){}
+
+    public void deleteItem(){}
+
+    public void saveChanges(){}
+
+    public void switchSceneToOrderCatalog(){}
+
+    public void logOut(){}
+
+
+
+
+
+
+
+
+
+}
+
