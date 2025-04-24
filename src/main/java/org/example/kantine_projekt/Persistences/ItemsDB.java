da19bd48308cf51a08831d4e6d9bcbb79a518d59
Jakob2rune <jakjes01@easv365.dk>
2025-04-24 13:48:55 +0200

    Update Item

--- src/main/java/org/example/kantine_projekt/Persistences/ItemsDB.java
+++ src/main/java/org/example/kantine_projekt/Persistences/ItemsDB.java
@@ -85,6 +85,22 @@ public class ItemsDB implements ItemsDBInterface {
 
     @Override
     public void updateItem(Item item) {
+        String sp = "{call update_item(?,?,?,?,?,?)}";
+        Connection conn = DBConnection.getConnection();
+        try (CallableStatement cstmt = conn.prepareCall(sp)){
+            cstmt.setInt(1, item.getItemId());
+            cstmt.setString(2, item.getName());
+            cstmt.setDouble(3, item.getUnitPrice());
+            cstmt.setInt(4,item.getMinimumQuantity());
+            cstmt.setInt(5, item.getCurrentQuantity());
+            cstmt.setInt(6, item.getReorderAmount());
+            cstmt.executeUpdate();
+
+        }catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("something went wrong in createItem");
+            throw new RuntimeException(e);
+        }
 
     }
 
